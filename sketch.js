245ab9c156c00dfee90ae5b6e9fcdd306cf772f7
Zarya Ajasin <irawo10@gmail.com>
2025-02-05 17:33:55 -0500

    updates

--- sketch.js
+++ sketch.js
@@ -4,12 +4,11 @@ let hands = [];
 let painting;
 let px = 0;
 let py = 0;
-let correct = false;
 let classifier;
 let canvas;
-let drawThis = "sheep";
 let result;
 let confidence;
+let mlGuess = 'im not sure what this is?';
 
 function preload() {
   handPose = ml5.handPose({ flipped: true });
@@ -26,8 +25,6 @@ function setup() {
   
   canvas = createCanvas(640, 480);
 
-  classifier.classifyStart(canvas, gotResult);
-
   // Create an off-screen graphics buffer for painting
   painting = createGraphics(640, 480);
   painting.clear();
@@ -35,82 +32,77 @@ function setup() {
   video = createCapture(VIDEO, { flipped: true });
   video.hide();
 
+  classifier.classifyStart(canvas, gotResult);
+
+
   // Start detecting hands
   handPose.detectStart(video, gotHands);
-
-  // pick thing to draw
-  let words = ["cat", "broccoli", "sweater", "lightning", "sheep"];
-  drawThis = random(words);
 }
 
 function draw() {
-  if (correct) {
-    background(0, 128, 0, 255);
-    image(painting, 0, 0);
-    textSize(24);
-    fill(255);
-    text('yay you drew ' + drawThis +'!', 20, 400); // prompt
-    text('refresh page to play again', 20, 400); // prompt
 
-  } else {
-    image(mCamera, 0, 0);
-    background(255, 255, 255, 180);
+  background(255, 255, 255);
+  //image(mCamera, 0, 0);
 
-    // Ensure at least one hand is detected
-    if (hands.length > 0) {
-      let hand = hands[0];
-      let index = hand.index_finger_tip;
-      let thumb = hand.thumb_tip;
 
-      // Compute midpoint between index finger and thumb
-      let x = (index.x + thumb.x) * 0.5;
-      let y = (index.y + thumb.y) * 0.5;
+  // Ensure at least one hand is detected
+  if (hands.length > 0) {
+    let hand = hands[0];
+    let index = hand.index_finger_tip;
+    let thumb = hand.thumb_tip;
 
-      // Draw only if fingers are close together
-      let d = dist(index.x, index.y, thumb.x, thumb.y);
-      if (d < 20) {
-        painting.stroke(0, 0, 0);
-        painting.strokeWeight(5);
-        painting.line(px, py, x, y);
-      }
+    // Compute midpoint between index finger and thumb
+    let x = (index.x + thumb.x) * 0.5;
+    let y = (index.y + thumb.y) * 0.5;
 
-      // Update previous position
-      px = x;
-      py = y;
-
-      // instructions
-      textSize(18);
-      text('your word is: ' + drawThis, 20, 400); // prompt
-      textSize(12);
-      text('Put a hand in frame and touch your pointer finger to thumb in order to start drawing', 20, 420);
-      text('remove your hand from the screen to clear', 20, 440);
-      text('try to accuratley draw what the ML model is looking for', 20, 460);
-
+    // Draw only if fingers are close together
+    let d = dist(index.x, index.y, thumb.x, thumb.y);
+    if (d < 40) {
+      painting.stroke(0, 0, 0);
+      painting.strokeWeight(3);
+      painting.line(px, py, x, y);
     } else {
-      painting.clear();
-
-      // instructions
-      textSize(18);
-      text('reverse pictionary!', 20, 400);
-      textSize(12);
-      text('Put a hand in frame and touch your pointer finger to thumb in order to start drawing', 20, 420);
-      text('remove your hand from the screen to clear', 20, 440);
-      text('try to accuratley draw what the ML model is looking for', 20, 460);
+      noStroke();
+      fill(225);
+      ellipse(index.x, index.y, 5, 5);
+      ellipse(thumb.x, thumb.y, 5, 5);
+      fill(0);
     }
 
-    image(painting, 0, 0);
+    // Update previous position
+    px = x;
+    py = y;
+
+    // instructions
+    textSize(18);
+    if(frameCount % 60 == 0){
+      mlGuess = 'i think its ' + result + '?';
+    }
+    text(mlGuess, 20, 400);
+    textSize(12);
+    text('Put a hand in frame and touch your pointer finger to thumb in order to start drawing', 20, 420);
+    text('remove your hand from the screen to clear', 20, 440);
+    text('try to see if the ML can guess it right!', 20, 460);
+
+  } else {
+    painting.clear();
+
+    // instructions
+    textSize(18);
+    text('reverse pictionary!', 20, 400);
+    textSize(12);
+    text('Put a hand in frame and touch your pointer finger to thumb in order to start drawing', 20, 420);
+    text('remove your hand from the screen to clear', 20, 440);
+    text('try to see if the ML can guess it right!', 20, 460);
   }
 
-  if (result == drawThis && confidence > 0.5) {
-
-  }
+  image(painting, 0, 0);
 
 }
 
 
 function gotResult(results) {
   // The results are in an array ordered by confidence.
-  
   result = results[0].label;
   confidence = results[0].confidence;
   console.log(result + ":" + confidence);
